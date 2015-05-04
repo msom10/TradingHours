ad952c80bdbbb3dd3ecb31b6386215a1762f4cf1
Mohanish <mohanish.soma@gmail.com>
2015-05-04 23:03:50 +1000

    SideBar implemented

---
+++ app/src/main/java/msoma/tradinghours/LoginFragment.java
@@ -0,0 +1,32 @@
+package msoma.tradinghours;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return rootView;
+    }
+}
